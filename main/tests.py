db21f88fbe9ce351fbe5cb6cb2f33dc6147c04ae
Ruturaj Mohite <mohite.ruturaj15@gmail.com>
2023-05-12 21:18:37 +0530

    Add course tests

--- main/tests.py
+++ main/tests.py
@@ -10,8 +10,13 @@ class CourseTestCase(TestCase):
 									 course_code = "TST 102")
 
 	def test_course_get(self):
-		course = Course.objects.get(course_code = "TST 101")
+		course = models.Course.objects.get(course_code = "TST 101")
 		self.assertEqual(course.course_name, "TestCourse1")
 
 	def test_course_create(self):
+		models.Course.objects.create(course_name = "TestCourse3",
+									 course_code = "TST 103")
+
+class ChapterTestCase(TestCase):
+	def setUp(self):
 		pass
