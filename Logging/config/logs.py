ae9dba758d201314a0424489be7ddd04a5a85bc7
Blackthorn <phanisriram02@gmail.com>
2023-05-15 04:08:59 +0530

    Fixed Logging and made it automated

--- Logging/config/logs.py
+++ Logging/config/logs.py
@@ -1,4 +1,5 @@
 import subprocess, time
+from datetime import datetime
 
 months = {"May":"05"}
 
@@ -8,10 +9,10 @@ while (True):
     output = subprocess.check_output('kubectl logs notesite-78b9b6f4b5-s5wkh', shell=True)
 
     # Open a file and write the output to it
-    with open('ScriptLogs.log', 'w') as f:
+    with open('backend1.log', 'w') as f:
         f.write(output.decode('utf-8'))
 
-    filename = 'ScriptLogs.log'
+    filename = 'backend1.log'
 
     # Read the contents of the file into a list
     with open(filename, 'r') as f:
@@ -30,10 +31,19 @@ while (True):
         date = fields[0]
         date_fields = date.split("/")
         month = months[date_fields[1]]
-        date = date_fields[0] + "/" + month + "/" + date_fields[2]
-        for j in range(1, len(fields)):
-            date += " " + fields[j]
-        lines[i] = date
+        date = date_fields[0][1:] + "/" + month + "/" + date_fields[2][:-1]
+        time_field = fields[1]
+        time_fields = time_field.split(":")
+        # print(date_fields[2][:len(date_fields[2])-1])
+        dt = datetime(int(date_fields[2][:len(date_fields[2])]), int(month), int(date_fields[0][1:]), int(time_fields[0]), int(time_fields[1]), int(time_fields[2][:-1]))
+        
+        dt = dt.isoformat()
+        dt += ".000Z"
+        for j in range(2, len(fields)):
+            fields[j] = fields[j].replace("\"", "")
+                
+            dt += " " + fields[j]
+        lines[i] = dt
 
     # Write the modified contents back to the file
     with open(filename, 'w') as f:
